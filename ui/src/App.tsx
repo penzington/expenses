c85f48ecaf1cfde06e19a2de343c7d07435c1eb1
Maciej Pekala <m@peka.la>
2019-03-07 18:43:23 +0100

    Connect frontend to the GraqhQL backend, example Grapqhl query

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { useQuery } from "urql";
+import logo from "./logo.svg";
+import "./App.css";
+
+const getExpenses = `
+  query GetExpenses($skip: Int!) {
+    expenses(skip: $skip) {
+      id
+      comment
+    }
+  }
+`;
+
+const ExpensesList = ({ skip = 10 }) => {
+  const [res] = useQuery({
+    query: getExpenses,
+    variables: { skip }
+  });
+
+  if (res.fetching || res.data === undefined) {
+    return <span>Loading...</span>;
+  } else if (res.error) {
+    return <span>Oh no!</span>;
+  }
+
+  return (
+    <ul>
+      {res.data.expenses.map(({ id }: { id: string }) => (
+        <li key={id}>{id}</li>
+      ))}
+    </ul>
+  );
+};
 
 class App extends Component {
   render() {
@@ -19,6 +50,7 @@ class App extends Component {
           >
             Learn React
           </a>
+          <ExpensesList skip={0} />
         </header>
       </div>
     );
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider, createClient } from "urql";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = createClient({
+  url: "http://localhost:8000/graphql"
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider value={client}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
